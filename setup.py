46b062270054663d80b23571900e13a0ae38fbe0
fraserwg <fraser.goldsworth@st-annes.ox.ac.uk>
2020-07-22 15:34:52 +0100

    formatting

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ setup(
     author_email='fraser.goldsworth@physics.ox.ac.uk',
     packages=['elePyant', 'elePyant.test'],
     scripts=[],  # ['bin/<<script_name>>]
-    data_files = ['elePyant/test/da_test.nc','elePyant/test/ds_test.nc'],
+    data_files=['elePyant/test/da_test.nc','elePyant/test/ds_test.nc'],
     url='https://github.com/fraserwg/elePyant/releases/tag/v0.0.2',
-    download_url = 'https://github.com/fraserwg/elePyant/archive/v0.0.2.tar.gz',
+    download_url='https://github.com/fraserwg/elePyant/archive/v0.0.2.tar.gz',
     license='LICENSE',
     description='Package that performs compression by rounding.',
     long_description=open('README.md').read(),
